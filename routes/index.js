3abdb8e48176192f5d37653b3efe0df8b1b1d0f6
Pato Sala <patosala@Patos-Mac-Pro.local>
2020-11-11 15:01:15 -0300

    mercadoliebre

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET register page */
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+/* GET login page */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+module.exports = router;
